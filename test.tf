30238b2f9633629915577f63f44908c792b0575e
Oleksandr Timoshenko <56413194+Tymoshenko1999@users.noreply.github.com>
2019-11-28 15:52:13 +0200

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,3 +1,11 @@
+
+provider "azurerm" {
+  Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+  version = "~>1.5"
+  profile = "StorageV2"
+}
+
+
 terraform {
   backend "azurerm" {
     storage_account_name  = "tstate3035"
