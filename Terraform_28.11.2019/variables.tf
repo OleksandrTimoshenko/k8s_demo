dc79fc9f73404dc733c5c375374123653ef4b2e9
Oleksandr Timoshenko <56413194+Tymoshenko1999@users.noreply.github.com>
2019-11-29 20:09:19 +0200

    Update variables.tf

--- Terraform_28.11.2019/variables.tf
+++ Terraform_28.11.2019/variables.tf
@@ -2,15 +2,16 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "subscription_id" {}
 variable "tenant_id" {}
+variable "ssh_public_key"{}
 
 
 variable "agent_count" {
     default = 1                     # Original Error: Code="QuotaExceeded" Message="Provisioning of resource(s) for container service k8stest in resource group azure-k8stest failed. Message: The operation couldn't be completed as it results in exceeding quota limit of Core.
 }
 
-variable "ssh_public_key" {
-    default = "/home/alex/.ssh/id_rsa.pub"
-}
+#variable "ssh_public_key" {
+#    default = "/home/alex/.ssh/id_rsa.pub"
+#}
 
 variable "dns_prefix" {
     default = "k8stest"
@@ -37,7 +38,7 @@ variable log_analytics_workspace_location {
     default = "westeurope"
 }
 
-# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing
 variable log_analytics_workspace_sku {
     default = "PerGB2018"
 }
